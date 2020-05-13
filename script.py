fa5f0122ba2a7cc1f84fa8ce3376f07c1205f8c1
Louis Popi <theh2o64@gmail.com>
2020-05-13 19:27:54 +0200

    rnn: Use our new RNN

--- script.py
+++ script.py
@@ -1,310 +1,243 @@
-# Please comment your code
-import json
-import re
-
-import tensorflow.compat.v1 as tf
+# -*- coding: utf-8 -*-
 import numpy as np
-import pandas as pd
-import nltk
-
-from nltk.corpus import stopwords
-
-nltk.download('punkt')
-nltk.download('stopwords')
-
-# Word2Vec model
-from gensim.models import KeyedVectors
-model_movie = KeyedVectors.load_word2vec_format("word2vec_movies_format.txt", binary=False)
-
-def movieDF():
-	# Open movie file
-	with open('../data.txt', 'r', errors='replace') as f:
-	    lines = (f.readlines())[:800]
-
-	sents=[]
-	for i in range(0,len(lines)-1,2):
-		q = lines[i][11:-1]
-		a = lines[i+1][9:-1]
-		sents.append([q,a])
-
-	return pd.DataFrame(sents, columns=['Question','Answer'])
-
-df_movie = movieDF()
-
-# Please comment your code
-import re
-import string
+import tensorflow as tf
 import pickle
+from tensorflow.keras import layers , activations , models , preprocessing
+from tensorflow.keras import preprocessing , utils
+import os
+import yaml
 
-def build_seq(df, model):
-	seq_data = []
-	whole_words = []
-	max_input_words_amount = 0
-	max_output_words_amount = 1
-	for index, row in df.iterrows():
-		question = row.Question.lower()
-		answer = row.Answer
-		seq_data.append([question,answer])
-		# we need to tokenise question 
-		for i in question:
-				if i in string.punctuation: 
-						question = question.replace(i," ")
-		for i in question:
-				if i in string.digits:
-						question = question.replace(i," ")
-		tokenized_q = question.split()
-		# we do not need to tokenise answer (because we implement N to One model)
-		# make a list with only one element (whole sentence)
-		tokenized_a =[answer]
-		
-		# add question list and answer list (one element)
-		whole_words += tokenized_q
-		whole_words += tokenized_a
-		
-		# we need to decide the maximum size of input word tokens
-		max_input_words_amount = max(len(tokenized_q), max_input_words_amount)
+# Import data
+import requests, zipfile, io
+r = requests.get( 'https://github.com/shubham0204/Dataset_Archives/blob/master/chatbot_nlp.zip?raw=true' ) 
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
 
-	# we now have a vacabulary list
-	unique_words = list(set(whole_words))
+# Preprocess the data
 
-	# adding special tokens in the vocabulary list
-	# _B_: Beginning of Sequence
-	# _E_: Ending of Sequence
-	# _P_: Padding of Sequence - for different size input
-	# _U_: Unknown element of Sequence - for different size input
-	unique_words.append('_B_')
-	unique_words.append('_E_')
-	unique_words.append('_P_')
-	unique_words.append('_U_')
+dir_path = 'chatbot_nlp/data'
+files_list = os.listdir(dir_path + os.sep)
 
-	num_dic = {n: i for i, n in enumerate(unique_words)}
+questions = list()
+answers = list()
 
-	dic_len = len(num_dic)
-		
-	return num_dic,dic_len,seq_data,max_input_words_amount,unique_words
+for filepath in files_list:
+    stream = open( dir_path + os.sep + filepath , 'rb')
+    docs = yaml.safe_load(stream)
+    conversations = docs['conversations']
+    for con in conversations:
+        if len( con ) > 2 :
+            questions.append(con[0])
+            replies = con[ 1 : ]
+            ans = ''
+            for rep in replies:
+                ans += ' ' + rep
+            answers.append( ans )
+        elif len( con )> 1:
+            questions.append(con[0])
+            answers.append(con[1])
 
-def get_target_a(sentence, num_dic):
-	tokenized_sentence = [sentence]
-	ids = []
-	for token in tokenized_sentence:
-		if token in num_dic:
- 			ids.append(num_dic[token])
-		else:
-			ids.append(num_dic['_U_'])
-	return ids
+answers_with_tags = list()
+for i in range( len( answers ) ):
+    if type( answers[i] ) == str:
+        answers_with_tags.append( answers[i] )
+    else:
+        questions.pop( i )
 
-def get_vectors_q(sentence, model, num_dic, max_):
-	# tokenise the sentence
-	max_input_words_amount = max_
-	sen = sentence.lower()
-	for i in sen:
-		if i in string.punctuation: 
-			sen = sen.replace(i," ")
-	for i in sen:
-		if i in string.digits:
-			sen = sen.replace(i," ")
-			
-	tokens =	sen.split()
-	tokenized_sentence = [w for w in tokens if not w in stopwords.words()]
-	 
-	diff = max_input_words_amount - len(tokenized_sentence)
-	
-	# add paddings if the word is shorter than the maximum number of words	
-	for x in range(diff):
-		tokenized_sentence.append('_P_')
-	data = tokens_to_ids(tokenized_sentence,model,num_dic)
-	return data
+answers = list()
+for i in range( len( answers_with_tags ) ) :
+    answers.append( '<START> ' + answers_with_tags[i] + ' <END>' )
 
-def get_vectors_a(sentence, model, num_dic):	
-	tokenized_sentence = [sentence]
-	data = tokens_to_ids(tokenized_sentence,model,num_dic)
-	return data
+tokenizer = preprocessing.text.Tokenizer()
 
-# convert tokens to index
-def tokens_to_ids(tokenized_sentence,model,num_dic):
-	ids = []
-	for token in tokenized_sentence:
-		try:
-			ids.append(model[token])
-		except KeyError as e:
-			ids.append(model['_U_'])
-	return ids
-
-seq_movie = build_seq(df_movie, model_movie)
-movie_dic,movie_len,movie_seq,movie_max,movie_unique = seq_movie
-movie_key = []
-for key in movie_dic:
-	movie_key.append(key)
-
-# generate a batch data for training/testing
-def make_batch(seq_data, model, num_dic, dic, max_):
-	input_batch = []
-	output_batch = []
-	target_batch = []
-
-	for seq in seq_data:	
-		# Input for encoder cell, convert question to vector
-		input_data = get_vectors_q(seq[0],model,num_dic,max_) 
-		# Input for decoder cell, Add '_B_' at the beginning of the sequence data
-		output_data = output_data = [model['_B_']] #[np.random.rand(100)]
-		output_data += get_vectors_a(seq[1],model,num_dic)
-		# Output of decoder cell (Actual result), Add '_E_' at the end of the sequence data
-		target = get_target_a(seq[1],dic)
-		target.append(dic['_E_'])
-		array_input=np.array(input_data)
-		array_output=np.array(output_data)
-		# Convert each token vector to one-hot encode data
-		input_batch.append(array_input)
-		output_batch.append(array_output)
-		target_batch.append(target)
-	return input_batch, output_batch, target_batch
-
-movie_key = []
-for key in movie_dic:
-	movie_key.append(key)
-
-def build(com_len):
-	learning_rate = 0.002
-	n_hidden = 128
-	n_class = com_len
-	n_input = 100
-
-	### Neural Network Model
-	tf.reset_default_graph()
-
-	# encoder/decoder shape = [batch size, time steps, input size]
-	tf.disable_eager_execution()
-	enc_input = tf.placeholder(tf.float32, [None, None, n_input])
-	dec_input = tf.placeholder(tf.float32, [None, None, n_input])
-
-	# target shape = [batch size, time steps]
-	targets = tf.placeholder(tf.int64, [None, None])
-
-	# Encoder Cell
-	with tf.variable_scope('encode'):
-		enc_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
-		enc_cell = tf.nn.rnn_cell.DropoutWrapper(enc_cell, output_keep_prob=0.5)
-		outputs, enc_states = tf.nn.dynamic_rnn(enc_cell, enc_input,
-												dtype=tf.float32)
-	# Decoder Cell
-	with tf.variable_scope('decode'):
-		dec_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
-		dec_cell = tf.nn.rnn_cell.DropoutWrapper(dec_cell, output_keep_prob=0.5)
-		# [IMPORTANT] Setting enc_states as inital_state of decoder cell
-		outputs, dec_states = tf.nn.dynamic_rnn(dec_cell, dec_input,
-													initial_state=enc_states,
-													dtype=tf.float32)
-
-	model = tf.layers.dense(outputs, n_class, activation=None)
-	cost = tf.reduce_mean(
-				tf.nn.sparse_softmax_cross_entropy_with_logits(
-					logits=model, labels=targets))
-	optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-	
-	sess = tf.Session()
-	sess.run(tf.global_variables_initializer())
-	return sess,model,cost,optimizer,enc_input,dec_input,targets
-
-
-'''
-sess,model,cost,optimizer,enc_input,dec_input,targets = build(movie_len)
-input_batch, output_batch, target_batch = make_batch(movie_seq, model_movie, movie_key, movie_dic, movie_max)
-
-saver_movie = tf.train.Saver()
-total_epoch = 5000
-
-for epoch in range(total_epoch):
-		_, loss = sess.run([optimizer, cost],
-											 feed_dict={enc_input: input_batch,
-																	dec_input: output_batch,
-																	targets: target_batch})
-		if epoch % 100 == 0:
-				print('Epoch:', '%04d' % (epoch + 1),
-							'cost =', '{:.6f}'.format(loss))
-
-print('Epoch:', '%04d' % (epoch + 1),
-			'cost =', '{:.6f}'.format(loss))
-print('Training completed')
-save_path_movie = saver_movie.save(sess,"save/model_movie.ckpt")
-'''
-
-'''
-import string
+# Prefilter before tokenizer
 import re
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    return text
 
-def prepro(que):
-	q = ""
-	que = que.lower()
-	for i in que:
-		if i in string.punctuation: 
-			que = que.replace(i," ")
-	for i in que:
-		if i in string.digits:
-			que = que.replace(i," ")
-	tokens = word_tokenize(que)
-	filtered_q = [w for w in tokens if not w in stopwords.words()]
-	for i in filtered_q:
-		q = q + i + " "
-	return q.strip()
+questions = [clean_text(s) for s in questions]
+answers = [clean_text(s) for s in answers]
 
-# Please comment your code
-def answer_com(sentence,max_output_words_amount):
-	
-	tf.reset_default_graph()
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
+tokenizer.fit_on_texts( questions + answers )
+VOCAB_SIZE = len( tokenizer.word_index )+1
+print( 'VOCAB SIZE : {}'.format( VOCAB_SIZE ))
 
-		learning_rate = 0.002
-		n_hidden = 128
-		n_class = movie_len
-		n_input = 100
-		enc_input = tf.placeholder(tf.float32, [None, None, n_input])
-		dec_input = tf.placeholder(tf.float32, [None, None, n_input])
-		targets = tf.placeholder(tf.int64, [None, None])
-		with tf.variable_scope('encode'):
-		  enc_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
-		  enc_cell = tf.nn.rnn_cell.DropoutWrapper(enc_cell, output_keep_prob=0.5)
-		  outputs, enc_states = tf.nn.dynamic_rnn(enc_cell, enc_input,
-												dtype=tf.float32)
-		with tf.variable_scope('decode'):
-		  dec_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
-		  dec_cell = tf.nn.rnn_cell.DropoutWrapper(dec_cell, output_keep_prob=0.5)
-		  outputs, dec_states = tf.nn.dynamic_rnn(dec_cell, dec_input,
-											  initial_state=enc_states,
-											  dtype=tf.float32)
-		model = tf.layers.dense(outputs, n_class, activation=None)
-		cost = tf.reduce_mean(
-				  tf.nn.sparse_softmax_cross_entropy_with_logits(
-					  logits=model, labels=targets))
-		optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-		saver_movie = tf.train.Saver()
-		saver_movie.restore(sess, "save/model_movie.ckpt")
-		
-		seq_data = [sentence, '_U_' * max_output_words_amount]
-		input_batch, output_batch, target_batch = make_batch([seq_data],model_movie,movie_key,movie_dic,movie_max)
-		prediction = tf.argmax(model, 2)
-		result = sess.run(prediction,
-						  feed_dict={enc_input: input_batch,
-									 dec_input: output_batch,
-									 targets: target_batch})
+# Preparing data for Seq2Seq model
+from gensim.models import Word2Vec
+import re
 
-		# convert index number to actual token 
-		decoded = [movie_unique[i] for i in result[0]]
-		# Remove anything after '_E_'		
-		if "_E_" in decoded:
-			end = decoded.index('_E_')
-			translated = ' '.join(decoded[:end])
-		else :
-			translated = ' '.join(decoded[:])
-	return translated
+vocab = []
+for word in tokenizer.word_index:
+    vocab.append( word )
 
-questions = ["Hi","Hello","I am so lonely", "Can you sleep?", "What is your age?", "I hate you", "Do you like me?", "You're so mean", "Can you drive?", "That's so bad", "what do you mean?", "oh my god"]
+def tokenize( sentences ):
+    tokens_list = []
+    vocabulary = []
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = re.sub( '[^a-zA-Z]', ' ', sentence )
+        tokens = sentence.split()
+        vocabulary += tokens
+        tokens_list.append( tokens )
+    return tokens_list , vocabulary
+
+p = tokenize( questions + answers )
+model = Word2Vec( p[ 0 ] + [["unk"] * 100], size=200, window=5, min_count=1, iter=10, workers=4)
+
+# Add a token for words which aren't in the model
+unknown_words = []
+for word in tokenizer.word_index:
+    if word not in model.wv.vocab:
+      unknown_words.append(word)
+vocab.append("unk")
 for q in questions:
-	print(q , ' ->', answer_com(prepro(q),movie_max))
-'''
+  for unk in unknown_words:
+    q.replace(unk, "<UNK>")
+for a in answers:
+  for unk in unknown_words:
+    a.replace(unk, "<UNK>")
+
+embedding_matrix = np.zeros( ( VOCAB_SIZE , 200 ) )
+for i in range( len( tokenizer.word_index ) ):
+    if (vocab[i] in unknown_words):
+      embedding_matrix[ i ] = model.wv[ "unk" ]
+    else:
+      embedding_matrix[ i ] = model.wv[ vocab[i] ]
+
+# encoder_input_data
+tokenized_questions = tokenizer.texts_to_sequences( questions )
+maxlen_questions = max( [ len(x) for x in tokenized_questions ] )
+padded_questions = preprocessing.sequence.pad_sequences( tokenized_questions , maxlen=maxlen_questions , padding='post' )
+encoder_input_data = np.array( padded_questions )
+print( encoder_input_data.shape , maxlen_questions )
+
+# decoder_input_data
+tokenized_answers = tokenizer.texts_to_sequences( answers )
+maxlen_answers = max( [ len(x) for x in tokenized_answers ] )
+padded_answers = preprocessing.sequence.pad_sequences( tokenized_answers , maxlen=maxlen_answers , padding='post' )
+decoder_input_data = np.array( padded_answers )
+print( decoder_input_data.shape , maxlen_answers )
+
+# decoder_output_data
+tokenized_answers = tokenizer.texts_to_sequences( answers )
+for i in range(len(tokenized_answers)) :
+    tokenized_answers[i] = tokenized_answers[i][1:]
+padded_answers = preprocessing.sequence.pad_sequences( tokenized_answers , maxlen=maxlen_answers , padding='post' )
+onehot_answers = utils.to_categorical( padded_answers , VOCAB_SIZE )
+decoder_output_data = np.array( onehot_answers )
+print( decoder_output_data.shape )
+
+# Defining the Encoder-Decoder model
+encoder_inputs = tf.keras.layers.Input(shape=( None , ))
+encoder_embedding = tf.keras.layers.Embedding( VOCAB_SIZE, 200 , weights=[embedding_matrix] ) (encoder_inputs)
+encoder_outputs , state_h , state_c = tf.keras.layers.LSTM( 200 , return_state=True )( encoder_embedding )
+encoder_states = [ state_h , state_c ]
+
+decoder_inputs = tf.keras.layers.Input(shape=( None ,  ))
+decoder_embedding = tf.keras.layers.Embedding( VOCAB_SIZE, 200 , weights=[embedding_matrix]) (decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM( 200 , return_state=True , return_sequences=True)
+decoder_outputs , _ , _ = decoder_lstm ( decoder_embedding , initial_state=encoder_states )
+decoder_dense = tf.keras.layers.Dense( VOCAB_SIZE , activation=tf.keras.activations.softmax ) 
+output = decoder_dense ( decoder_outputs )
+
+model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output )
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy')
+
+model.summary()
+
+# Training the model
+model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=64, epochs=300 ) 
+model.save( 'models/1/model.h5' )
+
+# Defining inference models
+def make_inference_models():
+    
+    encoder_model = tf.keras.models.Model(encoder_inputs, encoder_states)
+    
+    decoder_state_input_h = tf.keras.layers.Input(shape=( 200 ,))
+    decoder_state_input_c = tf.keras.layers.Input(shape=( 200 ,))
+    
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    
+    decoder_outputs, state_h, state_c = decoder_lstm(
+        decoder_embedding , initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = tf.keras.models.Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
+    
+    return encoder_model , decoder_model
+
+def save_inference_models():  
+    encoder_model, decoder_model = make_inference_models()
+    encoder_model.save( 'models/1/model_enc.h5' )
+    decoder_model.save( 'models/1/model_dec.h5' )
+
+def save_tokenizer():
+    with open('models/1/tokenizer.pickle', 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_inference_models(enc_file, dec_file):
+    encoder_model = tf.keras.models.load_model( enc_file )
+    decoder_model = tf.keras.models.load_model( dec_file )
+    return encoder_model , decoder_model
+
+def load_tokenizer(tokenizer_file):
+    with open(tokenizer_file, 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+# Save everything
+save_inference_models()
+save_tokenizer()
+
+# Talking with our Chatbot
+def str_to_tokens( sentence : str ):
+    words = sentence.lower().split()
+    tokens_list = list()
+    for word in words:
+        tokens_list.append( tokenizer.word_index[ word ] ) 
+    return preprocessing.sequence.pad_sequences( [tokens_list] , maxlen=maxlen_questions , padding='post')
+
+enc_model , dec_model = make_inference_models()
+
+for _ in range(10):
+    states_values = enc_model.predict( str_to_tokens( input( 'Enter question : ' ) ) )
+    empty_target_seq = np.zeros( ( 1 , 1 ) )
+    empty_target_seq[0, 0] = tokenizer.word_index['start']
+    stop_condition = False
+    decoded_translation = ''
+    while not stop_condition :
+        dec_outputs , h , c = dec_model.predict([ empty_target_seq ] + states_values )
+        sampled_word_index = np.argmax( dec_outputs[0, -1, :] )
+        sampled_word = None
+        for word , index in tokenizer.word_index.items() :
+            if sampled_word_index == index :
+                decoded_translation += ' {}'.format( word )
+                sampled_word = word
+        
+        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+            stop_condition = True
+            
+        empty_target_seq = np.zeros( ( 1 , 1 ) )  
+        empty_target_seq[ 0 , 0 ] = sampled_word_index
+        states_values = [ h , c ] 
+
+    print( decoded_translation [:-4]) # remove end word
